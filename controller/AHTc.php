05876b21602e2b08e5d2e0a28ddcdfc644ddae89
rekagyorke <reka_gyorke@yahoo.com>
2015-08-17 10:39:56 +0300

    setup

---
+++ controller/AHTc.php
@@ -0,0 +1,188 @@
+<?php 
+	$cities = array("A0"=>"Athens, Greece" , "A1"=>"Paris, France" ,"A2"=>"Rome, Italy" ,"A3"=>"Barcelona, Spain" , "A4"=>"Prague, Czech Rep");
+	
+	$price = array(
+				array( 1   , 3 , 2   , 2   , 0.5  ),
+				array( 0.33, 1 , 0.5 , 0.5 , 0.33 ),
+				array( 0.5 , 2 , 1   , 2   , 0.33 ),
+				array( 0.5 , 2 , 0.5 , 1   , 0.5  ),
+				array( 2   , 3 , 3   , 2   , 1    ));
+	
+	$distance = array(
+					array( 1    , 3    , 2    , 5 , 0.33 ),
+					array( 0.33 , 1    , 0.5  , 4 , 0.25 ),
+					array( 0.5  , 2    , 1    , 4 , 0.33 ),
+					array( 0.2  , 0.25 , 0.25 , 1 , 0.16 ),
+					array( 3    , 4    , 3    , 6 , 1    ));
+	
+	$entertainment = array(
+						array( 1 , 0.5 , 0.5 , 0.5 , 0.5 ),
+						array( 2 , 1   , 2   , 2   , 2   ),
+						array( 2 , 0.5 , 1   , 0.5 , 0.5 ),
+						array( 2 , 0.5 , 2   , 1   , 2   ),
+						array( 2 , 0.5 , 2   , 0.5 , 1   ));						
+	
+/***function to compute the sum of matrix columns, from the initial matrix+***/
+	function sum($a) {	
+		$sums = array();
+		$nrows = count($a);        
+		$ncols = count($a[0]); 
+		// compute sum of matrix columns  
+		for ( $j=0; $j < $ncols; $j++ ) {
+			$sums[$j] = 0.0;
+			for( $i=0; $i < $nrows; $i++ ) {
+				$sums[$j] += $a[$i][$j];
+			}
+		}
+	return $sums;
+	}
+   
+	$sum_p = sum($price);
+	// print_r ($sum_p);	  
+	$sum_d = sum($distance);
+	// print_r ($sum_d);  
+	$sum_e = sum($entertainment);
+	// print_r ($sum_e);
+	
+/***********function that computes the normalized values of initial matrix values*****************/	
+	function norm($a,$v){
+		$nrows = count($a);        
+		$ncols = count($a[0]); 
+		for ( $i=0; $i<$nrows; $i++ ) {
+			for( $j=0; $j<$ncols; $j++ ) {
+				if ($v[$i] != 0)
+				$a_norm[$j][$i] = $a[$j][$i]/$v[$i];
+				}
+			}
+	return $a_norm;
+	}
+	
+	$norm_p = norm($price,$sum_p);
+	// print_r ($norm_p);
+	$norm_d = norm($price,$sum_d);
+	// print_r ($norm_d);
+	$norm_e = norm($price,$sum_e);
+	// print_r ($norm_e);
+	 
+/**********Makes the sum of the rows***********************/
+	function sumfunc($a) {
+		$sums = array();
+		$nrows = count($a);
+		for ( $i=0; $i < $nrows; $i++ ) {
+			$sums[$i] += array_sum($a[$i]);
+			$sums[$i] =  $sums[$i]/$nrows;
+		}
+	 return $sums;  
+	}	
+									  
+	$sumf_p = sumfunc($norm_p);
+	// print_r ($sumf_p);
+	$sumf_d = sumfunc($norm_d);
+	// print_r ($sumf_d);
+	$sumf_e = sumfunc($norm_e);
+	// print_r ($sumf_e);
+
+  /***********Matrix for three (price, distance, entertainment) criteria***********/
+	$critPDE = array(
+				array( 1  , 3  ,4 ),
+				array( 1/3, 1  ,3 ),
+				array( 1/4, 1/3,1 ));	
+				
+				
+	$sum_crit = sum($critPDE);
+	//print_r ($sum_crit);  
+	$norm_crt = norm($critPDE,$sum_crit);
+	//print_r ($norm_crt); 
+	$sumf_crtPDE = sumfunc($norm_crt);
+	// print_r($sumf_crtPDE);
+
+/**********Matrix for two criteria (price and distance)***********/		 
+	$critPD = array(
+				array( 1  , 3 , 0 ),
+				array( 1/3, 1 , 0 ),
+				array( 0  , 0 , 0 ));	
+				
+	$sum_crit = sum($critPD);
+	//print_r ($sum_crit); 
+	$norm_crt = norm($critPD,$sum_crit);
+	// print_r ($norm_crt);  
+	$sumf_crtPD = sumfunc($norm_crt);
+	//print_r($sumf_crtPD);
+
+/**********Matrix for two criteria (price and entertainment)***********/
+	$critPE = array(
+				array( 1  , 0 , 4 ),
+				array( 0  , 0 , 0 ),
+				array( 1/4, 0 , 1 ));	
+							
+	$sum_crit = sum($critPE);
+	// print_r ($sum_crit);
+	$norm_crt = norm($critPE,$sum_crit);
+	// print_r ($norm_crt);
+	$sumf_crtPE = sumfunc($norm_crt);
+	// print_r($sumf_crtPE);
+	 
+ /**********Matrix for two criteria (distance and entertainment)***********/
+	$critDE = array(
+				array( 0 , 0  , 0 ),
+				array( 0 , 1  , 3 ),
+				array( 0 , 1/3, 1 ));	
+			
+	$sum_crit = sum($critDE);
+	// print_r ($sum_crit);  
+	$norm_crt = norm($critDE,$sum_crit);
+	// print_r ($norm_crt); 
+	$sumf_crtDE = sumfunc($norm_crt);
+	// print_r($sumf_crtDE);
+			
+/***************Compute decision matrix from alternative vectors**************************************/				
+	$result = array();
+	for ($l=0; $l<3; $l++){			
+		if ( $l==0 ) {
+			$v = $sumf_p;						
+		} elseif ( $l==1 ) {
+			$v = $sumf_d;
+		}  elseif ($l == 2) {
+			$v = $sumf_e;
+		}
+															
+		$i=0;
+		foreach ($v as $key => $value) {
+							
+			$result[$i][$l] = $value;
+			$i++;
+		}						
+	}			
+	//print_r ($result);
+	
+/**********************checking for check-box options and calculating decision criteria***************************************/
+	if(isset($_POST['price'])&&($_POST['distance'])&&($_POST['entertainment'])){
+		$crit_vector = $sumf_crtPDE;
+	} elseif(isset($_POST['price'])&&($_POST['distance'])) {
+		$crit_vector = $sumf_crtPD;
+	} elseif (isset($_POST['price'])&&($_POST['entertainment'])) {
+		$crit_vector = $sumf_crtPE;
+	}elseif (isset($_POST['distance'])&&($_POST['entertainment'])) {
+		$crit_vector = $sumf_crtDE;
+	} else {echo "Choose at least 2"; $error=1; } 
+	
+	for($i=0;$i<5;$i++){
+		for($j=0;$j<3;$j++){
+			$decision[$i][$j] = $result[$i][$j]* $crit_vector[$j];
+		}
+	}	
+	//print_r($decision);
+	
+	for($i=0;$i<5;$i++){
+		for($j=0;$j<3;$j++){
+			$final['A'.$i] = $final['A'.$i] + $decision[$i][$j];
+		}	
+	}		
+			
+	//print_r($final);
+	if (!$error) {
+		arsort($final);	
+		foreach ($final as $key => $value)
+			echo $cities[$key]."<br>";
+	}
+?>
---
+++ forms/culture.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Love to Travel</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" />
+	<link href="../css/bootstrap.css" rel="stylesheet"/>
+</head>
+<body>
+
+    <!-- NAVBAR
+    ================================================== -->
+   
+    
+    <div class="navbar-wrapper">
+        <div class="container">
+
+            <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin-bottom: 0px;">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.html"><b>Love to Travel</b></a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li><a href="../index.html">Home</a></li>
+                            <li><a href="../gallery.html">Gallery</a></li>
+                             <li><a href="http://www.booking.com/index.ro.html?sid=4fd9e8f62350c46f952de1cc54777587;dcid=4">Booking</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    
+	<div>
+	<h1 class="ToDo">What's important for you?<h1>
+	<p class="Do">Choose at least 2:</p>
+	<form action="_culture.php" method="post">
+	<table class="checkbox-table">
+		<tr>
+			<td>
+			<input type="checkbox" name="price" value="Price"><label>Price</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<input type="checkbox" name="distance" value="distance"><label>Distance</label>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			<input type="checkbox" name="entertainment" value="entertainment"><label>Entertainment</label>
+			</td>
+		</tr>
+		<tr>
+		<td>
+		<input type="submit" name="submit" value="Submit"/>	
+	</form>
+	</td>
+	</tr>
+	</table>
+	
+	
+	<table class="img-table"><tr>
+		<td>
+		<img src="..\css\travel2.jpg"/>
+		</td></tr>
+	</table>
+	</div>
+	
+        <!-- FOOTER -->
+        <div class="mastfoot" style="position:fixed; bottom: 5px;left:30px">
+            <p class="pull-right" style="position:fixed; right:30px"><a href="#">Back to top</a></p>
+            <p>&copyTravel </p>
+        </footer>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../js/jquery-1.9.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="ie10-viewport-bug-workaround.js"></script>
+
+    <!-- jssor slider scripts-->
+    <!-- use jssor.js + jssor.slider.js instead for development -->
+    <!-- jssor.slider.mini.js = (jssor.js + jssor.slider.js) -->
+    <script type="text/javascript" src="../js/jssor.slider.mini.js"></script>
+    <script>
+        jQuery(document).ready(function ($) {
+
+            var options = {
+                $FillMode: 2,                                       //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actual size, 5 contain for large image, actual size for small image, default value is 0
+                $AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+                $AutoPlayInterval: 4000,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+                $PauseOnHover: 1,                                   //[Optional] Whether to pause when mouse over if a slider is auto playing, 0 no pause, 1 pause for desktop, 2 pause for touch device, 3 pause for desktop and touch device, 4 freeze for desktop, 8 freeze for touch device, 12 freeze for desktop and touch device, default value is 1
+
+                $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
+                $SlideEasing: $JssorEasing$.$EaseOutQuint,          //[Optional] Specifies easing for right to left animation, default value is $JssorEasing$.$EaseOutQuad
+                $SlideDuration: 800,                               //[Optional] Specifies default duration (swipe) for slide in milliseconds, default value is 500
+                $MinDragOffsetToSlide: 20,                          //[Optional] Minimum drag offset to trigger slide , default value is 20
+                //$SlideWidth: 600,                                 //[Optional] Width of every slide in pixels, default value is width of 'slides' container
+                //$SlideHeight: 300,                                //[Optional] Height of every slide in pixels, default value is height of 'slides' container
+                $SlideSpacing: 0, 					                //[Optional] Space between each slide in pixels, default value is 0
+                $DisplayPieces: 1,                                  //[Optional] Number of pieces to display (the slideshow would be disabled if the value is set to greater than 1), the default value is 1
+                $ParkingPosition: 0,                                //[Optional] The offset position to park slide (this options applys only when slideshow disabled), default value is 0.
+                $UISearchMode: 1,                                   //[Optional] The way (0 parellel, 1 recursive, default value is 1) to search UI components (slides container, loading screen, navigator container, arrow navigator container, thumbnail navigator container etc).
+                $PlayOrientation: 1,                                //[Optional] Orientation to play slide (for auto play, navigation), 1 horizental, 2 vertical, 5 horizental reverse, 6 vertical reverse, default value is 1
+                $DragOrientation: 1,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+              
+                $BulletNavigatorOptions: {                          //[Optional] Options to specify and enable navigator or not
+                    $Class: $JssorBulletNavigator$,                 //[Required] Class to create navigator instance
+                    $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                    $Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+                    $Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+                    $SpacingX: 8,                                   //[Optional] Horizontal space between each item in pixel, default value is 0
+                    $SpacingY: 8,                                   //[Optional] Vertical space between each item in pixel, default value is 0
+                    $Orientation: 1,                                //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+                    $Scale: false,                                  //Scales bullets navigator or not while slider scale
+                },
+
+                $ArrowNavigatorOptions: {                           //[Optional] Options to specify and enable arrow navigator or not
+                    $Class: $JssorArrowNavigator$,                  //[Requried] Class to create arrow navigator instance
+                    $ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+                    $AutoCenter: 2,                                 //[Optional] Auto center arrows in parent container, 0 No, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+                    $Steps: 1                                       //[Optional] Steps to go for each navigation request, default value is 1
+                }
+            };
+
+            //Make the element 'slider1_container' visible before initialize jssor slider.
+            $("#slider1_container").css("display", "block");
+            var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+            //responsive code begin
+            //you can remove responsive code if you don't want the slider scales while window resizes
+            function ScaleSlider() {
+                var bodyWidth = document.body.clientWidth;
+                if (bodyWidth)
+                    jssor_slider1.$ScaleWidth(Math.min(bodyWidth, 1920));
+                else
+                    window.setTimeout(ScaleSlider, 30);
+            }
+            ScaleSlider();
+
+            $(window).bind("load", ScaleSlider);
+            $(window).bind("resize", ScaleSlider);
+            $(window).bind("orientationchange", ScaleSlider);
+            //responsive code end
+        });
+    </script>
+</body>
+</html>
